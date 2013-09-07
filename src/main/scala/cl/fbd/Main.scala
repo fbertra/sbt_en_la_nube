532509db70ba977731f7add8ba5bfb8b4e14fb4b
Francois Bertrand <fbertra@gnail.com>
2013-09-07 13:18:30 -0400

    hola mundo 3

---
+++ src/main/scala/cl/fbd/Main.scala
@@ -0,0 +1,13 @@
+package cl.fbd
+
+object Main {
+    def main (args : Array [String]) = {
+        println ("hola mundo")
+        
+        prueba
+    }
+    
+    def prueba () = {
+        println ("ctrl-o no funciona")
+    }
+}
